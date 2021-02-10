33e04587f8de6d12fda8f30fc6f4a33c47f5e1a5
yagamuu <ygmpeer@gmail.com>
2021-02-10 15:29:45 +0900

    feat: Add timer compornent and add timer in DS1 layout

--- src/graphics/DS_1/main.vue
+++ src/graphics/DS_1/main.vue
@@ -20,6 +20,7 @@
       :social="currentRunner.social"
       :displaySocial="currentSocial"
     />
+    <timer :position="timer" :time="timerReplicant.time" :timerStatus="timerReplicant.state"/>
   </game-layout>
 </template>
 
@@ -29,13 +30,14 @@ import { ClipPath } from '@/types/ClipPath';
 import { Getter } from 'vuex-class';
 import { Runner } from '@/types/Runners';
 import { Social } from '@/types/Social';
-import type { RunData, Timer } from '@/types/schemas/speedcontrol';
+import type { RunData, Timer as TimerInterface } from '@/types/schemas/speedcontrol';
 import { ComponentPosition } from '@/types/ComponentPosition';
 import GameLayout from '../views/GameLayout.vue';
 import EventLogo from '../components/EventLogo.vue';
 import RunInformationOneLine from '../components/RunInformationOneLine.vue';
 import Estimate from '../components/Estimate.vue';
 import Nameplate from '../components/Nameplate.vue';
+import Timer from '../components/Timer.vue';
 
 @Component({
   components: {
@@ -44,11 +46,12 @@ import Nameplate from '../components/Nameplate.vue';
     Estimate,
     RunInformationOneLine,
     Nameplate,
+    Timer,
   },
 })
 export default class extends Vue {
   @Getter readonly currentRunData!: RunData;
-  @Getter readonly timerReplicant!: Timer;
+  @Getter readonly timerReplicant!: TimerInterface;
   @Getter readonly currentRunner!: Runner;
   @Getter readonly commentatorsNameOneLine!: string;
   @Getter readonly commentatorsSocialOneLine!: Social;
@@ -67,12 +70,6 @@ export default class extends Vue {
       width: 620 - (5 * 2),
       height: 460 - (5 * 2),
     }, // DS-sub
-    {
-      x: 480 + 5,
-      y: 780 + 5,
-      width: 280 - (5 * 2),
-      height: 90 - (5 * 2),
-    }, // Timer
   ];
 
   gameTitle: ComponentPosition = {
@@ -107,6 +104,13 @@ export default class extends Vue {
     top: '980px',
     left: '960px',
     width: '930px',
+    height: '80px',
+  };
+
+  timer: ComponentPosition = {
+    top: '780px',
+    left: '480px',
+    width: '270px',
     height: '80px',
   };
 }
